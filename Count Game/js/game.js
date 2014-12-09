3e767c417256c6dbd35ce102c02d3065c346bb02
add1993 <adobhal93@gmail.com>
2014-12-09 10:29:41 +0530

    Learning shapes...

--- Count Game/js/game.js
+++ Count Game/js/game.js
@@ -6,6 +6,20 @@ c.Game = (function() {
 		this.canvas = document.getElementById('game-canvas');
 		this.stage = new createjs.Stage(this.canvas);
 
+		var shape = new createjs.Shape();
+		shape.graphics.setStrokeStyle(1);
+		shape.graphics.beginStroke("#000");
+		shape.graphics.beginFill("#efefef");
+
+		shape.graphics.rect(0,0,80,80);
+
+		this.stage.addChild(shape);
+
+ 		this.stage.update();
 	}
 	return countgame;
 })();
+
+window.onload = function() {
+	var game = new c.Game();
+}
